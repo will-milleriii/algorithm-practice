30b8c7a9352ef90756c4badb15166aeab55deeeb
Will Miller <willmiller@Wills-Air.fios-router.home>
2021-03-17 10:25:16 -0400

    beginning subsequence problem

--- src/main/java/Arrays/TwoNumberSum.java
+++ src/main/java/Arrays/TwoNumberSum.java
@@ -1,4 +1,23 @@
 package Arrays;
 
 public class TwoNumberSum {
+
+    public static void main(String[] args){
+
+        System.out.println(twoNumSum(new int[] {3, 5, -4, 8, 11, -1, 6}, 10));
+    }
+
+    public static int[] twoNumSum(int[] array, int targetSum){
+        for (int i = 0; i < array.length - 1; i++){
+            int firstVal = array[i];
+            for (int j = i + 1; j < array.length; j++){
+                int secondVal = array[j];
+                if (firstVal + secondVal == targetSum){
+                    return new int[] {firstVal, secondVal};
+                }
+            }
+        }
+        return new int[0];
+    }
+
 }
--- src/test/java/ArraysTests/TwoNumberSumTest.java
+++ src/test/java/ArraysTests/TwoNumberSumTest.java
@@ -1,4 +1,20 @@
 package ArraysTests;
 
+import org.junit.Test;
+
 public class TwoNumberSumTest {
+
+    @Test
+    public void twoNumSumTest1(){
+        //given
+        int[] array = new int[] {3, 5, -4, 8, 11, 1, -1, 6};
+        int targetSum = 10;
+
+        //when
+
+
+    }
+
+
+
 }
